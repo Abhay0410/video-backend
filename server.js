6efb1ad3bad599df53b415c7fdcfba71f6d7497a
Abhay0410 <kumarsinghabhay77@gmail.com>
2025-08-17 20:15:43 +0530

    server.js

--- server.js
+++ server.js
@@ -56,7 +56,8 @@ app.use(notFound);
 app.use(errorHandler);
 
 // Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`✅ Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`✅ Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+// });
+export default app;
